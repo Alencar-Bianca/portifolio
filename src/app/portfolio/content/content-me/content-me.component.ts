63195caa2ed57c54e6e52afac6293d76734e9dc7
Alencar-Bianca <73860563+Alencar-Bianca@users.noreply.github.com>
2024-06-02 14:30:26 -0300

    feito a logica de aparecer as informaoes de acordo com o link clicado

--- src/app/portfolio/content/content-me/content-me.component.ts
+++ src/app/portfolio/content/content-me/content-me.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+
 import { ContentAboutMeComponent } from '../content-about-me/content-about-me.component';
+import { ContentResumeComponent } from '../content-resume/content-resume.component';
 
 @Component({
   selector: 'app-content-me',
   standalone: true,
-  imports: [ContentAboutMeComponent],
+  imports: [ContentAboutMeComponent, ContentResumeComponent],
   templateUrl: './content-me.component.html',
   styleUrl: './content-me.component.scss'
 })
-export class ContentMeComponent {
+export class ContentMeComponent implements OnInit, OnDestroy {
+
+
+  public isShowInfo = signal<string>('resume');
+
+  public about = signal<boolean>(true);
+  public resume = signal<boolean>(false);
+  public project = signal<boolean>(false);
+
+  public showInfoAbout(info:string , event: Event) {
+    event.preventDefault();
+    this.isShowInfo.set(info);
+
+    console.log(info, this.isShowInfo());
+  }
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+
+  }
 
 }
